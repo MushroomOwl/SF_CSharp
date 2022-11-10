6ac0fb074524fbf62fd5ea01a5bd0caca29acfd3
MushroomOwl <55347320+MushroomOwl@users.noreply.github.com>
2022-11-11 01:38:55 +0300

    Reworked Task 1 to work with recursive function

--- 8_6/FinalTask/Task1/Program.cs
+++ 8_6/FinalTask/Task1/Program.cs
@@ -1,8 +1,76 @@
-﻿using System.IO;
-using static System.Net.WebRequestMethods;
-
-class Program
+﻿class Program
 {
+    public static void RecursiveRemove(DirectoryInfo directory)
+    {
+        DateTime now = DateTime.Now;
+        DirectoryInfo[] dirs = directory.GetDirectories();
+        foreach (DirectoryInfo dir in dirs)
+        {
+            RecursiveRemove(dir);
+            TimeSpan timeSinceLastAccessed = now - dir.LastAccessTime;
+            Console.WriteLine("Directory {0} accessed {1} minutes ago", dir.ToString(), timeSinceLastAccessed.TotalMinutes);
+            if (timeSinceLastAccessed.TotalMinutes <= 1)
+            {
+                Console.WriteLine("Skipping...");
+                continue;
+            }
+            Console.WriteLine("Removing...");
+            try
+            {
+                dir.Delete();
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Can't completely remove directory - not found exception: {0}", ex.Message);
+                continue;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Can't completely remove directory - access denied: {0}", ex.Message);
+                continue;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can't completely remove directory - unexpected error: {0}", ex.Message);
+                continue;
+            }
+            Console.WriteLine("Done");
+        }
+
+        FileInfo[] files = directory.GetFiles();
+        foreach (FileInfo file in files)
+        {
+            TimeSpan timeSinceLastAccessed = now - file.LastAccessTime;
+            Console.WriteLine("File {0} accessed {1} minutes ago", file.ToString(), timeSinceLastAccessed.TotalMinutes);
+            if (timeSinceLastAccessed.TotalMinutes <= 1)
+            {
+                Console.WriteLine("Skipping...");
+                continue;
+            }
+            Console.WriteLine("Removing...", file.ToString());
+            try
+            {
+                file.Delete();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Can't remove file - not found exception: {0}", ex.Message);
+                continue;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Can't remove file - access denied: {0}", ex.Message);
+                continue;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can't remove file - unexpected error: {0}", ex.Message);
+                continue;
+            }
+            Console.WriteLine("Done");
+        }
+    }
+
     public static void Main()
     {
         string? folderPath;
@@ -32,99 +100,23 @@ class Program
         }
 
         DirectoryInfo directory;
-        DirectoryInfo[] dirs;
-        FileInfo[] files;
         try
         {
             directory = new DirectoryInfo(folderPath);
-            dirs = directory.GetDirectories();
-            files = directory.GetFiles();
+            RecursiveRemove(directory);
+            Console.WriteLine("Directory cleaned from old files and folders data");
         }
         catch (DirectoryNotFoundException ex)
         {
             Console.WriteLine("Unexpected file not found exception: {0}", ex.Message);
-            return;
         }
         catch (UnauthorizedAccessException ex)
         {
             Console.WriteLine("Can't access directory: {0}", ex.Message);
-            return;
         }
         catch (Exception ex)
         {
             Console.WriteLine("Unexpected error accessing directory: {0}", ex.Message);
-            return;
         }
-
-        DateTime now = DateTime.Now;
-
-        foreach (DirectoryInfo dir in dirs) {
-            TimeSpan timeSinceLastAccessed = now - dir.LastAccessTime;
-            Console.WriteLine("Directory {0} accessed {1} minutes ago", dir.ToString(), timeSinceLastAccessed.TotalMinutes);
-            if (timeSinceLastAccessed.TotalMinutes <= 30) {
-                Console.WriteLine("Skipping...");
-                continue;
-            }
-            Console.WriteLine("Removing...");
-            Console.ReadLine();
-            try
-            {
-                // It wasn't clear whether in Task 1 we should use recursive function
-                // or should check only folder access time. If we should then there's little
-                // to nothing difference between Task 1 and Task 3, so just for sake of variation I'll leave it here
-                dir.Delete(true);
-            }
-            catch (DirectoryNotFoundException ex)
-            {
-                Console.WriteLine("Can't completely remove directory - not found exception: {0}", ex.Message);
-                continue;
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine("Can't completely remove directory - access denied: {0}", ex.Message);
-                continue;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Can't completely remove directory - unexpected error: {0}", ex.Message);
-                continue;
-            }
-            Console.WriteLine("Done");
-        }
-
-        foreach (FileInfo file in files)
-        {
-            TimeSpan timeSinceLastAccessed = now - file.LastAccessTime;
-            Console.WriteLine("File {0} accessed {1} minutes ago", file.ToString(), timeSinceLastAccessed.TotalMinutes);
-            if (timeSinceLastAccessed.TotalMinutes <= 30)
-            {
-                Console.WriteLine("Skipping...");
-                continue;
-            }
-            Console.ReadLine();
-            Console.WriteLine("Removind...", file.ToString());
-            try
-            {
-                file.Delete();
-            }
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine("Can't remove file - not found exception: {0}", ex.Message);
-                continue;
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine("Can't remove file - access denied: {0}", ex.Message);
-                continue;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Can't remove file - unexpected error: {0}", ex.Message);
-                continue;
-            }
-            Console.WriteLine("Done");
-        }
-
-        Console.WriteLine("Directory cleaned from old files and folders data");
     }
 }
