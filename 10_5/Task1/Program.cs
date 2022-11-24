1732704b97f8b58c608d4b55e1dc9942fdb4c768
MushroomOwl <55347320+MushroomOwl@users.noreply.github.com>
2022-11-24 22:35:22 +0300

    Interfaces Task 1 / Task 2 draft.

---
+++ 10_5/Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿class Program {
+    interface ISummarizer<T> {
+        T Sum(T x, T y);
+    }
+
+    interface ISubtractor<T>
+    {
+        T Subtract(T x, T y);
+    }
+
+    interface IMultiplier<T>
+    {
+        T Multiply(T x, T y);
+    }
+
+    class CalcInt : ISummarizer<int>, ISubtractor<int>, IMultiplier<int> {
+        int ISummarizer<int>.Sum(int x, int y) {
+            return x + y;
+        }
+        int ISubtractor<int>.Subtract(int x, int y) {
+            return x - y;
+        }
+        int IMultiplier<int>.Multiply(int x, int y) {
+            return x * y;
+        }
+    }
+
+    interface IConsoleReader {
+        int ReadValue();
+    }
+
+    class ConsoleReaderInt : IConsoleReader {
+        public int ReadValue() {
+            bool firstTry = true;
+            while (true) {
+                try {
+                    Console.Write(firstTry ? "Input int value: " : "Please input correct integer value: ");
+                    string? input = Console.ReadLine();
+                    return Convert.ToInt32(input);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("Input exception - {0}: {1}", ex.GetType(), ex.Message);
+                    firstTry = false;
+                }
+            }
+        }
+    }
+
+    static void Main()
+    {
+        ISummarizer<int> sumFunc = new CalcInt();
+        ISubtractor<int> subtractFunc = new CalcInt();
+        IMultiplier<int> multiplierFunc = new CalcInt();
+
+        IConsoleReader reader = new ConsoleReaderInt();
+        int x = reader.ReadValue();
+        int y = reader.ReadValue();
+
+        Console.WriteLine("x + y = {0}", sumFunc.Sum(x, y));
+        Console.WriteLine("x - y = {0}", subtractFunc.Subtract(x, y));
+        Console.WriteLine("x * y = {0}", multiplierFunc.Multiply(x, y));
+    }
+}
---
+++ 10_5/Task2/Program.cs
@@ -0,0 +1,91 @@
+﻿class Program
+{
+    interface ISummarizer<T>
+    {
+        T Sum(T x, T y, ILogger? logger);
+    }
+
+    interface ISubtractor<T>
+    {
+        T Subtract(T x, T y, ILogger? logger);
+    }
+
+    interface IMultiplier<T>
+    {
+        T Multiply(T x, T y, ILogger? logger);
+    }
+
+    class CalcInt : ISummarizer<int>, ISubtractor<int>, IMultiplier<int>
+    {
+        int ISummarizer<int>.Sum(int x, int y, ILogger? logger)
+        {
+            int result = x + y;
+            if (logger != null) logger.Event(string.Format("x + y = {0}", result));
+            return x + y;
+        }
+        int ISubtractor<int>.Subtract(int x, int y, ILogger? logger)
+        {
+            int result = x - y;
+            if (logger != null) logger.Event(string.Format("x - y = {0}", result));
+            return x - y;
+        }
+        int IMultiplier<int>.Multiply(int x, int y, ILogger? logger)
+        {
+            int result = x * y;
+            if (logger != null) logger.Event(string.Format("x * y = {0}", result));
+            return x * y;
+        }
+    }
+
+    interface IConsoleReader
+    {
+        int Read();
+    }
+
+    interface ILogger
+    {
+        void Event(string message);
+    }
+
+    class ConsoleProcessor : IConsoleReader, ILogger
+    {
+        public int Read()
+        {
+            bool firstTry = true;
+            while (true)
+            {
+                try
+                {
+                    Console.Write(firstTry ? "Input int value: " : "Please input correct integer value: ");
+                    string? input = Console.ReadLine();
+                    return Convert.ToInt32(input);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Input exception - {0}: {1}", ex.GetType(), ex.Message);
+                    firstTry = false;
+                }
+            }
+        }
+
+        public void Event(string message)
+        {
+            Console.WriteLine("LOG: " + message);
+        }
+    }
+
+    static void Main()
+    {
+        ISummarizer<int> sumFunc = new CalcInt();
+        ISubtractor<int> subtractFunc = new CalcInt();
+        IMultiplier<int> multiplierFunc = new CalcInt();
+
+        ConsoleProcessor consoleProc = new ConsoleProcessor();
+        int x = consoleProc.Read();
+        int y = consoleProc.Read();
+
+        sumFunc.Sum(x, y, consoleProc);
+        subtractFunc.Subtract(x, y, consoleProc);
+        multiplierFunc.Multiply(x, y, consoleProc);
+    }
+}
